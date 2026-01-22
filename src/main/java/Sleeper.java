1af5157466f11fb85f6c934056b1cc22fc24e382
junxiancheh <e1354852@u.nus.edu>
2026-01-22 14:26:05 +0800

    Add to store user's input and return a list

--- src/main/java/Sleeper.java
+++ src/main/java/Sleeper.java
@@ -3,15 +3,28 @@ import java.io.*;
 
 public class Sleeper {
     public static void main(String[] args) {
+        ArrayList<String> items = new ArrayList<>();
         System.out.println("    ____________________________________________________________\n"
                 + "    Hello! I'm Sleeper\n"
                 + "    What can I do for you?\n"
                 + "    ____________________________________________________________\n");
+
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
+
         while (!userInput.equals("bye")) {
+            if (userInput.equals("list")) {
+                System.out.println("    ____________________________________________________________\n");
+                for (int i = 0; i < items.size(); i++) {
+                    System.out.println("    " + (i + 1) + ": " + items.get(i) + "\n");
+                }
+                System.out.println("    ____________________________________________________________");
+            }
+
+            items.add(userInput);
+
             System.out.println("    ____________________________________________________________\n"
-                    + "    " + userInput + "\n"
+                    + "    added: " + userInput + "\n"
                     + "    ____________________________________________________________");
             userInput = scanner.nextLine();
         }
