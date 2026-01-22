157df57849d9b567cda7bff40ec1ff768b2d1dfe
junxiancheh <e1354852@u.nus.edu>
2026-01-22 16:56:43 +0800

    Add the ability to mark tasks as done or not done

--- src/main/java/Sleeper.java
+++ src/main/java/Sleeper.java
@@ -3,30 +3,60 @@ import java.io.*;
 
 public class Sleeper {
     public static void main(String[] args) {
-        ArrayList<String> items = new ArrayList<>();
+
+        ArrayList<Task> items = new ArrayList<>();
+
         System.out.println("    ____________________________________________________________\n"
                 + "    Hello! I'm Sleeper\n"
                 + "    What can I do for you?\n"
                 + "    ____________________________________________________________\n");
 
         Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
+        
 
-        while (!userInput.equals("bye")) {
+        while (true) {
+            String userInput = scanner.nextLine().trim();
+
+            if (userInput.equals("bye")) {
+                break;
+            }
+
             if (userInput.equals("list")) {
                 System.out.println("    ____________________________________________________________\n");
                 for (int i = 0; i < items.size(); i++) {
                     System.out.println("    " + (i + 1) + ": " + items.get(i) + "\n");
                 }
                 System.out.println("    ____________________________________________________________");
+                continue;
             }
 
-            items.add(userInput);
+            if (userInput.startsWith("mark ")) {
+                int index = Integer.parseInt(userInput.substring(5)) - 1;
+                items.get(index).markAsDone();
+
+                System.out.println("    ____________________________________________________________\n"
+                        + "    Nice! I've marked this task as done: \n"
+                        + "     " + items.get(index) + "\n"
+                        + "    ____________________________________________________________");
+                        continue;
+            }
+
+            if (userInput.startsWith("unmark ")) {
+                int index = Integer.parseInt(userInput.substring(7)) - 1;
+                items.get(index).markAsNotDone();
+
+                System.out.println("    ____________________________________________________________\n"
+                        + "    OK, I've marked this task as not done yet: \n"
+                        + "     " + items.get(index) + "\n"
+                        + "    ____________________________________________________________");
+                        continue;
+            }
+
+            items.add(new Task(userInput)); 
 
             System.out.println("    ____________________________________________________________\n"
                     + "    added: " + userInput + "\n"
                     + "    ____________________________________________________________");
-            userInput = scanner.nextLine();
         }
         System.out.println("    ____________________________________________________________\n"
                 + "    Bye. Hope to see you again soon!\n"
