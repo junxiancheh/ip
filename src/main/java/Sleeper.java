cfa325a53506df6d7a10756c377964b8e867dad0
junxiancheh <e1354852@u.nus.edu>
2026-01-22 14:03:47 +0800

    Improve skeletal version to echo and say bye

--- src/main/java/Sleeper.java
+++ src/main/java/Sleeper.java
@@ -1,15 +1,23 @@
+import java.util.*;
+import java.io.*;
+
 public class Sleeper {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("____________________________________________________________\n"
-                + " Hello! I'm Sleeper\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________");
+        System.out.println("    ____________________________________________________________\n"
+                + "    Hello! I'm Sleeper\n"
+                + "    What can I do for you?\n"
+                + "    ____________________________________________________________\n");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println("    ____________________________________________________________\n"
+                    + "    " + userInput + "\n"
+                    + "    ____________________________________________________________");
+            userInput = scanner.nextLine();
+        }
+        System.out.println("    ____________________________________________________________\n"
+                + "    Bye. Hope to see you again soon!\n"
+                + "    ____________________________________________________________");
     }
+
 }
